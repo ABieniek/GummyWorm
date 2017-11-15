ff33204b7caa134c988697785ae0277cffc20f1a
Alexander Bieniek <A.Bieniek789@gmail.com>
2017-11-15 16:27:14 -0600

    desktopController reads image from filesystem in loop

--- desktopController/desktopController.c
+++ desktopController/desktopController.c
@@ -2,47 +2,54 @@
 #include "SDL/SDL2_image-2.0.2/SDL_image.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
 	bool quit = false;
-	    SDL_Event event;
+	SDL_Event event;
 
-	    SDL_Init(SDL_INIT_VIDEO);
-			IMG_Init(IMG_INIT_JPG);
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_JPG);
 
-	    SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
-	        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
-	    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-			// find separate
-	    SDL_Surface * image = IMG_Load("image.jpg");
-	    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+		// find separate
+	SDL_Surface * image;// = IMG_Load("image.jpg");
+	SDL_Texture * texture;// = SDL_CreateTextureFromSurface(renderer, image);
+	   
+ 
+	while (!quit)
+	{
+		image = IMG_Load("image.jpg");
+		texture = SDL_CreateTextureFromSurface(renderer, image);
 
-	    while (!quit)
-	    {
-	        SDL_WaitEvent(&event);
+	
+		SDL_WaitEvent(&event);
 
-	        switch (event.type)
-	        {
-	            case SDL_QUIT:
-	                quit = true;
-	                break;
-	        }
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
 
-	        //SDL_Rect dstrect = { 5, 5, 320, 240 };
-	        //SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-	        SDL_RenderCopy(renderer, texture, NULL, NULL);
-	        SDL_RenderPresent(renderer);
-	    }
+		//SDL_Rect dstrect = { 5, 5, 320, 240 };
+		//SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	sleep(1);
+	}
 
-	    SDL_DestroyTexture(texture);
-	    SDL_FreeSurface(image);
-	    SDL_DestroyRenderer(renderer);
-	    SDL_DestroyWindow(window);
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 
-	 		IMG_Quit();
-	    SDL_Quit();
+	 	IMG_Quit();
+	SDL_Quit();
 
-	    return 0;
+	return 0;
 }
