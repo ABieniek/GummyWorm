837e591c43bd11ee16136810fa19c40cc6ee6c13
Alexander Bieniek <A.Bieniek789@gmail.com>
2017-12-12 19:49:06 -0600

    got continuous 'video streaming' working, still crashes and burns because of memory and inefficiency

--- desktopController/desktopController.c
+++ desktopController/desktopController.c
@@ -18,17 +18,20 @@
 #include <dirent.h>
 
 typedef struct{
-	int writebuffidx;
 	char** filenameLists[2];
 	int nameListLengths[2];
 	char* videoFileName;
+	int writebuffidx;
 	pthread_mutex_t* readlock;
+	pthread_barrier_t* barrier;
 	pthread_cond_t* writecond;
 	pthread_mutex_t* writelock;
 	bool* videoWritten;
 	char* outdir[2];
+	char* outdirstar[2];
 	char* outname[2];
 } displayArg;
+
 void* runDisplay(void* arg);
 void* runffmpeg(void* arg);
 
@@ -37,26 +40,30 @@ int main(int argc, char **argv)
 	serverArg* sa = malloc(sizeof(serverArg));
 	sa->portnumber = 2410;
 	sa->videoFileName = "gotvideo.webm";
-	pthread_t serverid;
-	//pthread_create(&serverid, NULL, runServer, sa);
+	 pthread_t serverid;
+	pthread_create(&serverid, NULL, runServer, sa);
 
 	displayArg* da = malloc(sizeof(displayArg));
 	da->writecond = sa->writecond = malloc(sizeof(pthread_cond_t));
 	da->writelock = sa->writelock = malloc(sizeof(pthread_mutex_t));
 	da->readlock = malloc(sizeof(pthread_mutex_t));
+	da->barrier = malloc(sizeof(pthread_barrier_t));
 	da->videoWritten = sa->videoWritten = malloc(sizeof(bool));
 	pthread_mutex_init(da->writelock, NULL);
 	pthread_cond_init(da->writecond, NULL);
 	pthread_mutex_init(da->readlock, NULL);
+	pthread_barrier_init(da->barrier, NULL, 2);
 	*(da->videoWritten) = false;
 	da->videoFileName = sa->videoFileName = "gotvideo.webm";
-	da->outdir[0] = "imagelist0";
-	da->outdir[1] = "imagelist1";
+	da->outdir[0] = "imagelist0/";
+	da->outdir[1] = "imagelist1/";
+	da->outdirstar[0] = "imagelist0/*";
+	da->outdirstar[1] = "imagelist1/*";
 	da->outname[0] = "imagelist0/frame%03d.jpg";
 	da->outname[1] = "imagelist1/frame%03d.jpg";
 	da->writebuffidx = 0;
-	pthread_t ffmpegid;
 
+	/*
 	da->nameListLengths[0] = 17;
 	da->filenameLists[0] = malloc(17*sizeof(char*));
 	da->filenameLists[0][0] = "imagelist0/frame001.jpg";
@@ -96,8 +103,10 @@ int main(int argc, char **argv)
 	da->filenameLists[1][15] = "imagelist1/frame016.jpg";
 	da->filenameLists[1][16] = "imagelist1/frame017.jpg";
 	da->filenameLists[1][17] = "imagelist1/frame018.jpg";
+	*/
 
-	//pthread_create(&ffmpegid, NULL, runffmpeg, da);
+	pthread_t ffmpegid;
+	pthread_create(&ffmpegid, NULL, runffmpeg, da);
 	pthread_t displayid;
 	pthread_create(&displayid, NULL, runDisplay, da);
 	pthread_exit(NULL);
@@ -135,23 +144,23 @@ void* runffmpeg(void* arg)
 		}
 		rewinddir(dirp);
 		char** strptr = da->filenameLists[da->writebuffidx]
-		= malloc(da->nameListLengths[da->writebuffidx] * sizeof(char*));
+			= malloc(da->nameListLengths[da->writebuffidx] * sizeof(char*));
 		int i = 0;
 		while ((entry = readdir(dirp)) != NULL)
 		{
 			if (entry->d_type == DT_REG)
 			{
 				strptr[i] = malloc(strlen(entry->d_name)+1);
-				strcpy(strptr[i], entry->d_name);
+				strcpy(strptr[i], da->outdir[da->writebuffidx]);
+				strcat(strptr[i], entry->d_name);
 				i++;
 			}
 		}
 		*(da->videoWritten) = false;
 		pthread_cond_signal(da->writecond);
 		pthread_mutex_unlock(da->writelock);
-		pthread_mutex_lock(da->readlock);
 		da->writebuffidx = (da->writebuffidx+1)%2;
-		pthread_mutex_unlock(da->readlock);
+		pthread_barrier_wait(da->barrier);
 	}
 	return NULL;
 }
@@ -170,12 +179,16 @@ void* runDisplay(void* arg)
 		// find separate
 	SDL_Surface * image;// = IMG_Load("image.jpg");
 	SDL_Texture * texture;// = SDL_CreateTextureFromSurface(renderer, image);
+	//SDL_Texture* streamTexture = SDL_CreateTexture(renderer,
+	//	SDL_PIXELFORMAT_UNKNOWN, SDL_TEXTUREACCESS_STREAMING, 640, 480);
 	//texture = SDL_CreateTextureFromSurface(renderer, image);
 
 	// I want to show the n images evenly over 1 second
+	//	int pitch = 480;
 	struct timespec screendur;
 	bool quit = false;
-	int buffidx = 0;
+	int buffidx;
+	pthread_barrier_wait(da->barrier);
 	while (!quit)
 	{
 		/*
@@ -186,22 +199,24 @@ void* runDisplay(void* arg)
 		}
 		pthread_mutex_unlock(da->writelock);
 		*/
-		/*
+
 		if (da->writebuffidx == 1) // we're gonna read from buffer 0
 			buffidx = 0;
 		else if (da->writebuffidx == 0) // we're gonna read from buffer 1
 			buffidx = 1;
-			*/
-		buffidx = (buffidx+1)%2;
+		// buffidx = (buffidx+1)%2;
 		if (da->nameListLengths[buffidx] == 0) continue;
 		screendur.tv_nsec = 1000000000 / da->nameListLengths[buffidx];
-		int imgidx;
-		for (imgidx = 0; imgidx < da->nameListLengths[buffidx]; imgidx++)
+
+		int imgidx; for (imgidx = 0; imgidx < da->nameListLengths[buffidx]; imgidx++)
 		{
-			image = IMG_Load(da->filenameLists[buffidx][imgidx]);
-			texture = SDL_CreateTextureFromSurface(renderer, image);
 			// @TODO clean up texture loading to avoid memory leaks
-			SDL_WaitEvent(&event);
+			image = IMG_Load(da->filenameLists[buffidx][imgidx]);
+			//SDL_LockTexture(streamTexture, NULL, (void**)image, &pitch);
+
+			//SDL_UnlockTexture(streamTexture);
+			texture = SDL_CreateTextureFromSurface(renderer, image);
+			//SDL_WaitEvent(&event);
 
 			switch (event.type)
 			{
@@ -214,7 +229,21 @@ void* runDisplay(void* arg)
 			SDL_RenderCopy(renderer, texture, NULL, NULL);
 			SDL_RenderPresent(renderer);
 			nanosleep(&screendur, NULL);
+			SDL_DestroyTexture(texture);
+			SDL_FreeSurface(image);
 		}
+		/*
+		pid_t p = fork();
+		if (p == 0)
+		{
+			execlp("rm", "rm", da->outdirstar[buffidx], NULL);
+			perror("");
+			exit(6);
+		}
+		waitpid(p, NULL, 0);
+		*/
+
+		pthread_barrier_wait(da->barrier);
 	}
 
 	SDL_DestroyTexture(texture);
